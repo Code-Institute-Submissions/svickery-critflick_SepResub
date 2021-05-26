512d6d5bbb69fd96082132dd2393f668d9834257
svickery <s.vickery@hotmail.co.uk>
2021-05-26 15:10:08 +0000

    app.route added for all pages in run.py

--- run.py
+++ run.py
@@ -5,6 +5,31 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/reviews")
+def reviews():
+    return render_template("reviews.html")
+
+
+@app.route("/current")
+def current():
+    return render_template("current.html")
+
+
+@app.route("/sign_up")
+def sign_up():
+    return render_template("sign_up.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(
         host = os.environ.get("IP", "0.0.0.0"),
